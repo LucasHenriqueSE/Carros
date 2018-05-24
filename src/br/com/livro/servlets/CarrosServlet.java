5ca5838713990f2d48e514b1c98c5458f3590d12
Lucas Henrique De Sousa <lucashenrique@brq.com>
2018-05-24 17:53:22 -0300

    Criando web service básico(servlet) que consulta e exibe uma lista de carros

---
+++ src/br/com/livro/servlets/CarrosServlet.java
@@ -0,0 +1,31 @@
+package br.com.livro.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.livro.domain.Carro;
+import br.com.livro.service.CarroService;
+
+@WebServlet("/carros/*")
+public class CarrosServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private CarroService service = new CarroService();
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Carro> carros = this.service.listar();
+		String carrosString = carros.toString();
+		resp.getWriter().write(carrosString);
+	}
+}
